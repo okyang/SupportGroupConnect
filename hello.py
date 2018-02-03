02436ec001443ab1a575b8a4d6bbccea46134489
Derek Feng <derekfeng97@gmail.com>
2018-02-03 00:03:54 -0800

    UploadingLoginSkeleton

---
+++ hello.py
@@ -0,0 +1,32 @@
+from flask import Flask
+from flask import Flask, flash, redirect, render_template, request, session, abort
+import os
+ 
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    if not session.get('logged_in'):
+        return render_template('login.html')
+    else:
+        return "Hi"
+
+@app.route('/login', methods=['POST'])
+def do_admin_login():
+    print(request.form())
+    if request.form['password'] == 'password':
+        session['logged_in'] = True
+    else:
+        flash('That\'s the wrong password!')
+    
+    return home()
+
+@app.route('/logout')
+def logout():
+    session['logged_in'] = False
+    return home()
+
+
+if __name__ == "__main__":
+    app.secret_key = os.urandom(12)
+    app.run(debug=True)#host='0.0.0.0', port=4000)
