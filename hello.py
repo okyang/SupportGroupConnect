68b9a4a34f92c32c2082218a5d95a9fc979d7599
Derek Feng <derekfeng97@gmail.com>
2018-02-03 03:36:27 -0800

    IHAVETHEWORKINGCODE

--- hello.py
+++ hello.py
@@ -1,4 +1,5 @@
-from flask import Flask, flash, redirect, render_template, request, session, abort, url_for, escape
+from flask import Flask
+from flask import Flask, flash, redirect, render_template, request, session, abort
 import os
 import psycopg2
 import sys
@@ -17,18 +18,12 @@ def do_admin_login():
 
 	con = psycopg2.connect("host='localhost' dbname='supportGroupConnect' user='postgres' password='password'")   
 	cur = con.cursor()
-  cur.execute("SELECT username  FROM users WHERE users.username = '" + request.form['username']+ "'")
-  
-  username = cur.fetchone()
-  
 	cur.execute("SELECT password  FROM users WHERE users.username = '" + request.form['username']+ "'")
 	
 	password = cur.fetchone()
 	
 	con.commit()
-  
-  session['username'] = request.form['username'] #currently putting username into the session (not checking if it exists in the date base)
-	
+
 	if password is not None and request.form['password'] == password[0]:
 		session['logged_in'] = True
 	else:
@@ -44,15 +39,14 @@ def loadcreateaccount():
 def createaccountforreal():
 	con = psycopg2.connect("host='localhost' dbname='supportGroupConnect' user='postgres' password='password'")   
 	cur = con.cursor()
-	cur.execute("INSERT INTO users (username, password) VALUES ('" + request.form['username'] + "', '" + request.form['password'] + "')")
+	cur.execute("INSERT INTO Users (username, password) VALUES ('" + request.form['username'] + "', '" + request.form['password'] + "')")
+	con.commit()
 	return render_template('login.html')
 
 @app.route('/logout')
 def logout():
-    # remove the username from the session if it's there
-    session.pop('username', None)
     session['logged_in'] = False
-    return redirect(url_for('home'))
+    return home()
 
 if __name__ == "__main__":
     app.secret_key = os.urandom(12)
