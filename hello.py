29ca1cc5a1a3dbc8b11a0bfa2a711e3d21382942
Joshua Cao <caoj11@uci.edu>
2018-02-03 17:32:11 -0800

    Combined all out stuffff

--- hello.py
+++ hello.py
@@ -5,6 +5,9 @@ import sys
 
 app = Flask(__name__)
 
+
+
+
 @app.route('/')
 def home():
 	if not session.get('logged_in'):
@@ -14,8 +17,27 @@ def home():
 		cur = con.cursor()
 		cur.execute("SELECT condition  FROM condition WHERE condition.id = '" +session['user_id']+"'")
 		q_condition = cur.fetchone()
+		
+		cur.execute("SELECT username FROM users INNER JOIN friends ON users.id = friends.second_id WHERE first_id='"
+						+ session['user_id'] + "'")
+		friendlist = []
+		row = cur.fetchone()
+		while row != None:
+			friendlist += row
+			row = cur.fetchone()
+			
+		#cur.execute("(SELECT username FROM users INNER JOIN condition ON '" + session['condition'] + "' = condition AND '"+ session['user_id'] +"' != condition.ID)")
+		cur.execute("SELECT username FROM users INNER JOIN condition ON users.id = condition.id WHERE condition.id !=" 
+						+ session['user_id'] + " AND condition = '" + session['condition'] + "'")
+		suggestionlist = []
+		row = cur.fetchone()
+		while row != None:
+			suggestionlist += row
+			row = cur.fetchone()
+		
 		con.close()
-		return render_template('portal.html', username = request.form['username'], condition = q_condition)
+		return render_template('portal.html', username = request.form['username'], condition = q_condition, friendlist = friendlist,
+								suggestionlist = suggestionlist)
 
 @app.route('/login', methods=['POST'])
 def do_admin_login():
@@ -30,10 +52,10 @@ def do_admin_login():
 		cur.execute("SELECT id  FROM users WHERE users.username = '" + request.form['username']+ "'")
 		user_id = cur.fetchone()
 	
-		#cur.execute("SELECT condition  FROM condition WHERE condition.id = '" str(ID) "'")
 		cur.execute("SELECT condition FROM condition INNER JOIN users ON " + str(user_id[0]) +" = condition.id")
 		condition = cur.fetchone()
 	
+		session['username'] = request.form['username']
 		session['logged_in'] = True
 		session['condition'] = condition[0]
 		session['user_id'] = str(user_id[0])
@@ -66,12 +88,7 @@ def createaccountforreal():
 def logout():
     session['logged_in'] = False
     return home()
- 
-@app.route('/login/chatroomcancer')
-def chatroomcancer():
-	print("user2:  ", request.form['username'])
-	return render_template('chatroomcancer.html') #username = request.form['username'])
 
 if __name__ == "__main__":
     app.secret_key = os.urandom(12)
-    app.run(debug=True, host='0.0.0.0', port=4000)
+    app.run(debug=True)#host='0.0.0.0', port=4000)
